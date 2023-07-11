6624b35c2caeb559ae7390670e6a113a956741b8
joinprakhar <118005176+joinprakhar@users.noreply.github.com>
2023-07-12 01:06:01 +0530

    Update postpage.jsx

--- src/pages/Post/postpage.jsx
+++ src/pages/Post/postpage.jsx
@@ -80,61 +80,6 @@ export default function PostPage() {
               )}
             </div>
             <div className={styles.detailpart}>
-
-              <div >
-                <Link to={`/profile/${postInfo?.author?._id}`}>
-                  <a>
-                    Author :{" "}
-                    {`${postInfo?.author?.firstName} 
-                ${postInfo?.author?.lastName}`}
-                  </a>
-                </Link>
-                <a>
-                  <time>{formatISO9075(new Date(postInfo?.createdAt))}</time>
-                </a>
-              </div>
-              <div className={styles.block}>
-                {" "}
-                {cookies?.access_token?.id === postInfo.author._id && (
-                  <div className={styles.button}>
-                    <Link className={styles.btn} to={`/edit/${postInfo?._id}`}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                        />
-                      </svg>
-                      <div className={styles.text}>Edit this post</div>
-                    </Link>
-                    <Link className={styles.btn} onClick={deletePost}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M7 6V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5zm6.414 8l1.768-1.768-1.414-1.414L12 12.586l-1.768-1.768-1.414 1.414L10.586 14l-1.768 1.768 1.414 1.414L12 15.414l1.768 1.768 1.414-1.414L13.414 14zM9 4v2h6V4H9z"
-                        />
-                      </svg>
-                      <div className={styles.text}>Delete this post</div>
-                    </Link>
-                  </div>
-                )}
-              </div>
-
               <div className="author">
                 <Link to={`/profile/${postInfo?.author?._id}`}>
                   Author :{" "}
