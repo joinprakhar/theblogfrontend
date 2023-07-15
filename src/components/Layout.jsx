2a4c1af66b68497492e36b8461408965c4e68fb8
joinprakhar <pandeylive@gmail.com>
2023-07-15 18:34:47 +0530

    test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = () => {
   return (
